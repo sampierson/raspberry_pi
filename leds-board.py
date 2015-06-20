f9714a97f45eb11bad29ea5f8759c327e241d001
Sam Pierson <sam@sampierson.com>
2015-06-19 21:51:12 -0700

    Turn on LEDs with Python

---
+++ leds-board.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import time
+
+try:
+    import RPi.GPIO as GPIO
+except RuntimeError:
+    print("Error importing RPi.GPIO!  This is probably because you need superuser privileges.  You can achieve this by using 'sudo' to run your script")
+
+GPIO.setmode(GPIO.BOARD)
+
+channels = [12, 16, 18]
+for channel in channels:
+    print "Activating channel", channel
+    GPIO.setup(channel, GPIO.OUT)
+    GPIO.output(channel, GPIO.HIGH)
+    time.sleep(2)
+
+GPIO.cleanup()
+
